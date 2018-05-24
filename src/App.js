b1ccbd3ef868b2a64ee9cb5c404c22349a3f9a9b
jmichot <jmichot@turner-industries.com>
2018-05-24 13:38:31 -0500

    chore: add react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header id="Header">
+          <h1>Chatastrophe</h1>
+          <img src="assets/icon.png" id="test-image" alt="heart" />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
