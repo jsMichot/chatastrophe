17a04429a2b6c13b7a19bbb3a2e9c3b298684a0d
jmichot <jmichot@turner-industries.com>
2018-06-14 15:30:31 -0500

    chore: add semantic-ui-react

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React, {Component} from 'react';
 import {hot} from 'react-hot-loader';
 
 import './styles';
+import LoginContainer from './features/auth/LoginContainer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header id="Header">
-          <h1>Chatastrophe</h1>
-          <img src="assets/icon.png" id="test-image" alt="heart" />
-        </header>
+      <div>
+        <LoginContainer />
       </div>
     );
   }
---
+++ src/features/auth/LoginContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export class LoginContainer extends Component {
+  render() {
+    return (
+      <header id="Header">
+        <h1>Chatastrophe</h1>
+        <img src="assets/icon.png" id="test-image" alt="heart" />
+      </header>
+    );
+  }
+}
+
+export default LoginContainer;
